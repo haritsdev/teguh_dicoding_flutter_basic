0d2c29874346f59be18ce66e63d090c8f5b9ec8e
haritsdev <teguhcodedev1@gmail.com>
2022-05-23 03:55:09 +0700

    Part 5 Macam macam button

--- teguh_dicoding_basic/lib/main.dart
+++ teguh_dicoding_basic/lib/main.dart
@@ -12,7 +12,13 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class FirstScreen extends StatelessWidget {
+class FirstScreen extends StatefulWidget {
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  String language;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,13 +33,66 @@ class FirstScreen extends StatelessWidget {
                 icon: const Icon(Icons.search, color: Colors.white),
                 onPressed: () {})
           ]),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Icon(Icons.share),
-          Icon(Icons.thumb_up),
-          Icon(Icons.thumb_down)
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              margin: EdgeInsets.all(10),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text("Elevated Button"),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              child: TextButton(
+                onPressed: () {},
+                child: Text("Text Button"),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              child: OutlinedButton(
+                onPressed: () {},
+                child: Text("Outlined Button"),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              child: IconButton(
+                icon: Icon(Icons.volume_down),
+                onPressed: () {},
+                tooltip: 'Decrease volume',
+              ),
+            ),
+            Container(
+                margin: EdgeInsets.all(10),
+                child: DropdownButton<String>(
+                  items: const <DropdownMenuItem<String>>[
+                    DropdownMenuItem<String>(
+                      value: 'Dart',
+                      child: Text('Dart'),
+                    ),
+                    DropdownMenuItem<String>(
+                      value: 'Kotlin',
+                      child: Text('Kotlin'),
+                    ),
+                    DropdownMenuItem<String>(
+                      value: 'Swift',
+                      child: Text('Swift'),
+                    )
+                  ],
+                  value: language,
+                  hint: Text('Select language'),
+                  onChanged: (String value) {
+                    setState(() {
+                      language = value;
+                    });
+                  },
+                ))
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
